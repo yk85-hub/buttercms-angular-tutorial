223b4d846980f353c60cde1ce680e299b5d5cfb2
yk85-hub <55225896+yk85-hub@users.noreply.github.com>
2020-03-19 20:22:59 +0300

    Update butterCMS.service.ts

--- src/app/services/butterCMS.service.ts
+++ src/app/services/butterCMS.service.ts
@@ -1,4 +1,4 @@
 import Butter from 'buttercms';
 
-export const butterService = Butter('your_api_token');
+export const butterService = Butter('ee4e7a6ec21334902d24cf8d8bb38589c20b06b9');
 // replace `your_api_token` with the token provided to you by ButterCMS
